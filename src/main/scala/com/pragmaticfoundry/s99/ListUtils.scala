2696cde6364eee9d32d6d84108250f4193e9b987
David Galichet <david.galichet@gmail.com>
2011-06-12 23:17:05 +0200

    P09 solution

--- src/main/scala/com/pragmaticfoundry/s99/ListUtils.scala
+++ src/main/scala/com/pragmaticfoundry/s99/ListUtils.scala
@@ -6,51 +6,59 @@ package com.pragmaticfoundry.s99
 
 object ListUtils {
 
+    // P01
     def last[T](l:List[T]):T = l match {
         case Nil => throw new NoSuchElementException("List is empty")
         case head::Nil => head
         case head::tail => last(tail)
     }
 
+    // P02
     def penultimate[T](l:List[T]):T = l.reverse match {
         case first::second::tail => second
         case _ => l.head
     }
 
+    // P03
     def nth[T](k:Int, l:List[T]):T = if (k == 0) l.head else nth(k - 1, l.tail)
 
+    // P04
     def length[T](l:List[T]):Long = l match {
         case Nil => throw new NoSuchElementException("List is empty")
         case head::Nil => 1
         case head::tail => 1 + length(tail)
     }
 
+    // P05
     def reverse[T](l:List[T]):List[T] = {
         var reverted:List[T] = Nil
         for (e <- l) reverted ::= e
         reverted
     }
 
+    // P06
     def isPalindrome[T](l:List[T]) = l.reverse == l
 
+    // P07
     def flatten[Any](l:List[_]):List[Any] =  l.reverse.foldLeft(List.empty[Any]) {
         case ( r, t:List[_] ) => flatten(t):::r
         case ( r, t:Any ) => t::r
     }
 
+    // P08
     def compress[T](l:List[T]):List[T] = l match {
         case Nil => Nil
         case head::Nil => List(head)
         case head::tail => if (head != tail.head) head::compress(tail) else compress(tail)
     }
 
-    def pack[T](l:List[T]):List[List[T]] = l match {
-        case Nil => Nil
-        case head::Nil => List(List(head))
-        case head::tail if head == tail.head => List(head)::pack(tail)
-        case head::tail => List(head)::pack(tail)
+    // P09
+    def pack[T](l:List[T]):List[List[T]] = packFirsts(l) match {
+        case ( Nil, Nil ) => Nil
+        case ( h, t ) => h::pack(t)
     }
 
+    // utility for P09
     def packFirsts[T](l:List[T]):(List[T], List[T]) = ( (List.empty[T], List.empty[T]) /: l) {
         case ( ( Nil, _ ), e ) => ( e::Nil, Nil )
         case ( (h, Nil), e ) if h.head == e => ( e::h, Nil )
--- src/test/scala/com/pragmaticfoundy/s99/ListUtilsTests.scala
+++ src/test/scala/com/pragmaticfoundy/s99/ListUtilsTests.scala
@@ -21,34 +21,46 @@ class ListUtilsTests extends Specification { def is =
         "be able to Flatten a nested list structure"                                ! p07^
         "be able to Eliminate consecutive duplicates of list elements"              ! p08^
         "be able to Pack consecutive duplicates of list elements into sublists"     ! p09^
-        "return a tuple ( List(equal elements)), tail"                              ! h01^
+        "return a tuple ( List(equal head elements), tail)"                         ! h01^
                                                                                     end
 /*        "be able to Run-length encoding of a list"                                  ! p10^
         "be able to Modified run-length encoding"                                   ! p11^
         "be able to Decode a run-length encoded list"                               ! p12^*/
 
     def p01 = { 8 === last(List(1, 1, 2, 3, 5, 8)) }
+
     def p02 = { 5 === penultimate(List(1, 1, 2, 3, 5, 8)) }
+
     def p03 = { 2 === nth(2, List(1, 1, 2, 3, 5, 8)) }
+
     def p04 = { 6 === ListUtils.length(List(1, 1, 2, 3, 5, 8)) }
+
     def p05 = { List(8, 5, 3, 2, 1, 1) === reverse(List(1, 1, 2, 3, 5, 8)) }
+
     def p06 = { isPalindrome(List(1)) and !isPalindrome(List(1, 2)) and isPalindrome(List(1, 2, 3, 2, 1)) }
+
     def p07 = { List(1, 2, 3) === flatten(List(1, 2, 3)) and
         List(1, 1, 2) === flatten(List(1, List(1, 2))) and
         List(1, 1, 2, 3, 5, 8) === flatten(List(List(1, 1), 2, List(3, List(5, 8)))) }
+
     def p08 = {
         List() === compress(List()) and List('a) === compress(List('a)) and
         List('a, 'b) === compress(List('a, 'b)) and
         List('a, 'b) === compress(List('a, 'a, 'b)) and
         List('a, 'b, 'c, 'a, 'd, 'e) === compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
     }
+
     def p09 = { List(List('a)) === pack(List('a)) and
         List(List('a), List('b), List('c)) === pack(List('a, 'b, 'c)) and
-        List(List('a, 'a), List('b), List('c)) === pack(List('a, 'a, 'b, 'c))
-    /* List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)) ===
-            pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))*/ }
+        List(List('a, 'a), List('b), List('c)) === pack(List('a, 'a, 'b, 'c)) and
+        List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)) ===
+            pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    }
+
     def p10 = { false /*encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))*/ }
+
     def p11 = { false /*encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))*/ }
+
     def p12 = { false /*decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))*/ }
 
     def h01 = {
